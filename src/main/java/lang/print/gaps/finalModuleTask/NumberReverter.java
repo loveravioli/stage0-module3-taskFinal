3031ad7f45f0ead27e03b20ecce96fa50510f44b
pomidorNagolove <harrier136@icloud.com>
2023-05-07 12:09:07 +0300

    done

--- src/main/java/lang/print/gaps/finalModuleTask/NumberReverter.java
+++ src/main/java/lang/print/gaps/finalModuleTask/NumberReverter.java
@@ -2,5 +2,13 @@ package lang.print.gaps.finalModuleTask;
 
 public class NumberReverter {
     public void revert(int number) {
+        int remainder = 0;
+        int reverse = 0;
+        while(number != 0) {
+            remainder = number % 10;
+            reverse = reverse * 10 + remainder;
+            number=number/10;
+        }
+        System.out.println(reverse);
     }
 }
